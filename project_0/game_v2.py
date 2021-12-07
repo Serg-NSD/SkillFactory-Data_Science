6eea8ad979d4a96bd2c6f50bc20fc75d65c99e29
Serg-NSD <nist2@yandex.ru>
2021-12-07 17:01:37 +0900

    edit readme.md

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -16,34 +16,13 @@ def random_predict(number: int = 1) -> int:
     """
     count = 0
 
-   # while True:
-
-    #count = 0    # Счётчик попыток
-    min_number = 1
-    max_number = 101
-
-    while count < 21:
-        count += 1
-        random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
-        predict_number = (min_number + max_number) // 2
-
-        if number > predict_number:
-            min_number = predict_number    # Переназначение нижней границы поиска
-
-        elif number < predict_number:
-            max_number = predict_number    # Переназначение верхней границы поиска
-
-        else:
-            print(f'Average count of attempts: {count}.')
-            break
-
-        '''
+    while True:
         count += 1
         predict_number = np.random.randint(1, 101)  # предполагаемое число
         if number == predict_number:
             break  # выход из цикла если угадали
-        '''
-
+        
+        
     return count
 
 
@@ -57,9 +36,9 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    # np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
-
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10))  # загадали список чисел
+    print(random_array)
     for number in random_array:
         count_ls.append(random_predict(number))
 
@@ -68,8 +47,8 @@ def score_game(random_predict) -> int:
     return score
 
 
-score_game(random_predict)
-
-# if __name__ == '__main__':
-# RUN
-#    score_game(random_predict)
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
+    
+    
--- project_1/game_Guess.py
+++ project_1/game_Guess.py
@@ -1,14 +1,12 @@
 """Игра 'Угадай число'.
-
-Компьютер загадывает число и затем угадывает его менее,  
-чем за 20 попыток.
+Компьютер загадывает число и затем угадывает его менее, чем за 20 попыток.
 
 """
 
 import numpy as np
 
 number = np.random.randint(1, 101)    # Компьютер загадывает число от 1 до 100  
-print("Hidden the number: ", number)
+print("\nЗагаданное число: ", number)
 
 
 def game_score(number: int = 1) -> int:
@@ -18,26 +16,26 @@ def game_score(number: int = 1) -> int:
         number (int, optional): Hidden the number. Defaults to 1.
 
     Returns:
-        int: Average count of attempts.
+        int: Count of attempts.
     """
     
-    count = 0    # Счётчик попыток
-    min_number = 1
-    max_number = 101
+    count = 0    # Счётчик попыток  
+    min_number = 1    # Нижняя граница поиска  
+    max_number = 101    # Верхняя граница поиска  
     
-    while count < 21:
+    while True:
         count += 1
-        random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+        random_array = np.random.randint(min_number, max_number, size=(1000))  # Загадали список чисел с учётом границ  
         predict_number = (min_number + max_number) // 2
-        
+        print(f'Попытка №: {count}   Число {predict_number}')
         if number > predict_number:
-            min_number = predict_number    # Переназначение нижней границы поиска
+            min_number = predict_number    # Переназначение нижней границы поиска  
             
         elif number < predict_number:
-            max_number = predict_number    # Переназначение верхней границы поиска
+            max_number = predict_number    # Переназначение верхней границы поиска  
             
         else:
-            print(f'Average count of attempts: {count}.')
+            print(f'Количество попыток: {count}\n')
             break
     
     return count
